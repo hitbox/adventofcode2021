5b35cdd1f476801d9dd63e26118811bf7d29756e
Carl Harris <elgoogemail2007@gmail.com>
2021-12-03 02:40:13 -0500

    add nsetbits func

--- adventofcode.py
+++ adventofcode.py
@@ -119,6 +119,15 @@ def mkmasks(nbits):
     masks = [1 << shift for shift in range(nbits)]
     return masks
 
+def nsetbits(n):
+    "Count set bits in `n`"
+    # no intuition for binary--had to look this up again.
+    count = 0
+    while n > 0:
+        count += n & 1
+        n >>= 1
+    return count
+
 def power_consumption(bnums, nbits):
     masks = mkmasks(nbits)
     # "first bit" in challenge refers to least significant bit (right-most).
